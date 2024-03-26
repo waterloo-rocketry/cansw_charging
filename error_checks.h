e1c1962c02edc5bf31e6cbb0c708b668abfc2a55
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2024-03-25 22:33:00 -0400

    Format all source code and import rocketlib

--- error_checks.h
+++ error_checks.h
@@ -1,9 +1,9 @@
 #ifndef ERROR_CHECKS_H
-#define	ERROR_CHECKS_H
-
-#include "canlib/message_types.h"
+#define ERROR_CHECKS_H
 
 #include <stdbool.h>
+
+#include "canlib/message_types.h"
 
 // for voltages within these ranges, a warning will be sent out over CAN
 // values were determined by the given voltage rail range
@@ -21,5 +21,4 @@ bool check_battery_voltage_error(void);
 bool check_battery_current_error(void);
 bool check_bus_current_error(void);
 
-#endif	/* ERROR_CHECKS_H */
-
+#endif /* ERROR_CHECKS_H */
